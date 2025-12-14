e22db3a77bde86a7043714276c349f87409acd59
DonnyWu <donny.wu4@gmail.com>
2025-12-13 23:50:58 -0500

    Feature--Created a request for a single game from json file

--- Program.cs
+++ Program.cs
@@ -24,8 +24,12 @@ List<GameDto> games = [
         new DateOnly(2009, 6, 25))
 ];
 
+// GET /games
 app.MapGet("games", () => games);
 
-app.MapGet("/", () => "Hello World!");
+// GET /games/1
+app.MapGet("games/{id}", (int id) => games.Find( game => game.Id == id));
 
-app.Run();
+
+// POST /games
+// app.MapPost
